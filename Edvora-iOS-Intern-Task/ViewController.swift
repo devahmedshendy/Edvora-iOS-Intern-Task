23d1461d9814847487bc49639b5e93c3dbb14d4d
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2022-02-05 01:17:16 +0200

    Initial Commit

---
+++ Edvora-iOS-Intern-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Edvora-iOS-Intern-Task
+//
+//  Created by Ahmed Shendy on 2/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
